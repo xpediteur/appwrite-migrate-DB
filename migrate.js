9c656c662f445a649e4e29e3be31126ed618207b
xpediteur <borde@myapp2go.de>
2022-10-21 16:50:40 +0200

    Update migrate.js
    removed secret

--- migrate.js
+++ migrate.js
@@ -11,13 +11,12 @@ const client = new sdk.Client();
 client
     .setEndpoint('https://bf2a562.online-server.cloud/v1') // Replace this with your endpoint
     .setProject('63446724a6d27e696227') // Replace this with your projectID
-    .setKey('07c14994a2e981137b93ac6d8b8531cded8541f5de329facf52ee8e578b45bfdf1ea644d6c42189d276915e30eae55d000f2d42c46ae970023a4198a9507b3f5c5402eb687577980fa626ab776646b7749305f72b1fc44f3c6dccf0d1960d132f52c21b36d126f66ec577c6b299f731fa30fad0e8512c35e652bfbefb81d2c2e') // Your secret API key
+    .setKey('blabla123xyz') // Your secret API key
 ;
 
 const database = new sdk.Databases(client, '63446ca755a041305f7f');
 
 const users = new sdk.Users(client);
-
 
 
 app.get("/", async(req, res) => {
@@ -29,19 +28,11 @@ app.get("/", async(req, res) => {
     }
 })
 
-app.get('/api/account', async(req, res) => {
-    let data = await users.list();
-    res.send(data);
-    console.log('api/account called!:', data)
-})
-
-
 app.get('/api/keys', async(req, res) => {
     let data = await database.listDocuments('63446ca755a041305f7f', '635256b413c28b9de0c8');
     res.send(data.documents);
     console.log('api/customer called!', data)
 })
-
 
 async function getKeys() {
     try {
